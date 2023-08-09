0004f6a92d2259e6ea6ceaff5a10007a22df0134
Andrew Newport <anewport@dev-10.com>
2023-08-09 08:33:22 -0400

    Repitition 8-9

--- week-01/exercises/repetition-exercises/src/Exercise08.java
+++ week-01/exercises/repetition-exercises/src/Exercise08.java
@@ -9,14 +9,14 @@ public class Exercise08 {
         // Run the code before you make changes to better understand current behavior.
         // The transformation from `while` to `do/while` should not change behavior.
 
-        System.out.println("Enter a name (Required): ");
-        String name = console.nextLine();
+        String name = "";
 
-        while (name.isBlank()) {
+        do {
             System.out.println("Enter a name (Required): ");
             name = console.nextLine();
-        }
+        } while (name.isBlank());
 
         System.out.println("Name: " + name);
+
     }
 }
