2fa6439edf27257bc494701207926773c6e0267b
Andrew Newport <anewport@dev-10.com>
2023-08-22 16:14:38 -0400

    memories data package

--- week-03/projects/memories/src/main/java/learn/memories/data/MemoryRepository.java
+++ week-03/projects/memories/src/main/java/learn/memories/data/MemoryRepository.java
@@ -1,4 +1,20 @@
 package learn.memories.data;
 
-public class MemoryRepository {
+import learn.memories.models.Memory;
+
+import java.util.List;
+
+public interface MemoryRepository {
+
+    List<Memory> findAll() throws DataAccessException;
+
+    Memory findById(int memoryId) throws DataAccessException;
+
+    List<Memory> findShareable(boolean shareable) throws DataAccessException;
+
+    Memory add(Memory memory) throws DataAccessException;
+
+    boolean update(Memory memory) throws DataAccessException;
+
+    boolean deleteById(int memoryId) throws DataAccessException;
 }
